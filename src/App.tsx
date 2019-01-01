ee12c59c29215f8345efad6a17ba9724ec7688a9
Stefanwullems <stefanwullems1998@gmail.com>
2019-01-01 22:55:21 +0100

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Route } from "react-router-dom";
 import AboutContainer from "./components/About/AboutContainer";
 import ExperienceContainer from "./components/Experience/ExperienceContainer";
 import ContactContainer from "./components/Contact/ContactContainer";
-import SuccessfullySentMessagePage from "./components/Contact/SuccessfullySentMessagePage";
+
 class App extends React.Component {
   public render() {
     return (
@@ -13,11 +13,7 @@ class App extends React.Component {
         <Route exact path="/" component={HomeContainer} />
         <Route path="/about" component={AboutContainer} />
         <Route path="/experience" component={ExperienceContainer} />
-        <Route exact path="/contact" component={ContactContainer} />
-        <Route
-          path="/contact/successful"
-          component={SuccessfullySentMessagePage}
-        />
+        <Route path="/contact" component={ContactContainer} />
       </div>
     );
   }
--- src/components/Contact/ContactContainer.tsx
+++ src/components/Contact/ContactContainer.tsx
@@ -4,12 +4,19 @@ import AppBarContainer from "../AppBar/AppBarContainer";
 import { RouteComponentProps } from "react-router";
 import ContactForm from "./ContactForm";
 
+import { Route } from "react-router-dom";
+import SuccessfullySentMessagePage from "./SuccessfullySentMessagePage";
+
 class ContactContainer extends React.Component<RouteComponentProps> {
   render() {
     return (
       <React.Fragment>
         <AppBarContainer page="contact" />
-        <ContactForm />
+        <Route exact path="/contact" component={ContactForm} />
+        <Route
+          path="/contact/successful"
+          component={SuccessfullySentMessagePage}
+        />
       </React.Fragment>
     );
   }
